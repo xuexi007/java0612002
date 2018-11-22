312c88921e38682b1a55e284679add67a0286c47
java0612 <test@126.com>
2018-11-22 14:22:09 +0800

    修改pay--delte

--- src/main/java/com/offcn/pay/payDemo.java
+++ src/main/java/com/offcn/pay/payDemo.java
@@ -3,7 +3,7 @@ package com.offcn.pay;
 public class payDemo {
 
 	public static void main(String[] args) {
-      System.out.println("新增支付系统");
+      System.out.println("新增支付系统---delete");
 	}
 
 }
